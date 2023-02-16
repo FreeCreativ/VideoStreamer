3606488334e0b8600fcbfdce90121f712b3790bc
Okafor Tochukwu <okaforjonathant@outlook.com>
2023-02-16 10:22:13 +0100

    added video

--- db/index.js
+++ db/index.js
@@ -1,3 +1,19 @@
-const pg = require('pg-promise');
-const db = pg('postgres://postgres:Dontobec12345@localhost:5432;/family');
-module.exports = db;
+const pgp = require('pg-promise')(/* options */)
+
+class Database {
+    constructor(username, password, database, host, port) {
+        this.username = username;
+        this.password = password;
+        this.database = database;
+        this.host = host;
+        this.port = port;
+    }
+
+    connect() {
+        return pgp(`postgres://${this.username}:${this.password}@${this.host}:${this.port}/${this.database}`)
+    }
+}
+
+let db = new Database('postgres', 'Dontobec12345', 'family', '127.0.0.1', '5432');
+const connection = db.connect();
+module.exports = connection;
--- routes/videos.js
+++ routes/videos.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const fs = require("fs");
-const db = require("db")
+const db = require("../db")
 
 router.post('/upload', (req, res) => {
     let videoFiles = req.post().files;
